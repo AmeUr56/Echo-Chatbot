ed75b5a781c0b7895e8ede2c686721fdc2904e1c
Nxwfel <nelxpe@gmail.com>
2024-11-08 21:34:20 +0100

    Configured Responsivness

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,11 @@ document.getElementById('menuButton').addEventListener('click', function() {
     const menuBar = document.getElementById('menuBar');
     menuBar.classList.toggle('active');
 });
+document.getElementById('close').addEventListener('click', function() {
+    const menuBar = document.getElementById('menuBar');
+    menuBar.classList.toggle('active');
+});
+document.getElementById('Contact').addEventListener('click', function() {
+    const menuBar = document.getElementById('menuBar');
+    menuBar.classList.toggle('active');
+});
